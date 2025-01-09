0a3f0a23ee4b268720645c169a995c19dff5cb73
ckr <ckr3453@gmail.com>
2025-01-09 14:55:32 +0900

    feat: 상품 등록 request 추가

---
+++ src/main/java/me/ckr/productorderservice/product/AddProductRequest.java
@@ -0,0 +1,18 @@
+package me.ckr.productorderservice.product;
+
+import org.springframework.util.Assert;
+
+/**
+ * packageName : me.ckr.productorderservice.product
+ * fileName    : AddProductRequest
+ * author      : ckr
+ * date        : 25. 1. 9.
+ * description :
+ */
+record AddProductRequest(String name, int price, DiscountPolicy discountPolicy) {
+    AddProductRequest {
+        Assert.hasText(name, "상품명은 필수입니다.");
+        Assert.isTrue(price > 0, "상품 가격은 0보다 커야합니다.");
+        Assert.notNull(discountPolicy, "할인 정책은 필수입니다.");
+    }
+}
