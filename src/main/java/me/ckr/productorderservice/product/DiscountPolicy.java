80a78649d64443fb7c448f14c2fd13d1255c6a24
ckr <ckr3453@gmail.com>
2025-01-09 14:56:01 +0900

    feat: 할인 정책 추가

---
+++ src/main/java/me/ckr/productorderservice/product/DiscountPolicy.java
@@ -0,0 +1,12 @@
+package me.ckr.productorderservice.product;
+
+/**
+ * packageName : me.ckr.productorderservice.product
+ * fileName    : DiscountPolicy
+ * author      : ckr
+ * date        : 25. 1. 9.
+ * description :
+ */
+enum DiscountPolicy {
+    NONE
+}
