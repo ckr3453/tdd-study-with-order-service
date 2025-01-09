8c84b4853e3c2d08dbe774a79981b1cb82685135
ckr <ckr3453@gmail.com>
2025-01-09 14:54:46 +0900

    init project

---
+++ src/main/java/me/ckr/productorderservice/product/ProductPort.java
@@ -0,0 +1,13 @@
+package me.ckr.productorderservice.product;
+
+/**
+ * packageName : me.ckr.productorderservice.product
+ * fileName    : ProductPort
+ * author      : ckr
+ * date        : 25. 1. 9.
+ * description :
+ */
+interface ProductPort {
+
+    void save(final Product product);
+}
