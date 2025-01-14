2895e9616903be9a5641731e074328e4b14f3867
ckr <ckr3453@gmail.com>
2025-01-14 18:54:40 +0900

    test: 통합 테스트를 위해 시퀀스 초기화 설정 추가

--- src/test/java/me/ckr/productorderservice/DatabaseCleanup.java
+++ src/test/java/me/ckr/productorderservice/DatabaseCleanup.java
@@ -23,7 +23,7 @@ import java.util.stream.Collectors;
  */
 
 @Component
-public class DatabaseClenaup implements InitializingBean {
+public class DatabaseCleanup implements InitializingBean {
     @PersistenceContext
     private EntityManager entityManager;
 
